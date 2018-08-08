312c634c9d63d6832022f8e7bac85a477e123c59
Noor <noor19-meet@meet.mit.edu>
2018-08-08 07:13:35 -0400

    ted helps the best

--- exercises/app.py
+++ exercises/app.py
@@ -4,6 +4,14 @@ app = Flask(__name__)
 
 @app.route('/')
 def home():
-    return 'START HERE: CHANGE ME'
+    return render_template("home.html")
 
+@app.route('/student/<string:student_id>')
+def display_student(student_id):
+    student = query_by_id(student_id)
+    return render_template("student.html",
+    student_id=student_id,
+    student=student)
+
+    
 app.run(debug=True)
